9c92fac7fda00aec2d5451001853bbf87f538188
nenadn8 <nenadn8@gmail.com>
2024-11-20 22:42:40 +0100

    Added first lines of code.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox("C:\SeleniumDrivers\geckodriver.exe")
-driver.get("http://www.python.org")
+# The following section sets up Webdriver and implicit wait.
+options = FirefoxOptions()
+options.binary_location = r"D:\Firefox\firefox.exe"
+driver = webdriver.Firefox(options = options)
+driver.implicitly_wait(5)    # seconds
+
+# Basic operations and home page opening.
+home_page = "https://www.letskodeit.com/"
+driver.maximize_window()
+driver.get(home_page)
+
+# Signing in.
+sign_in_button = driver.find_element(By.XPATH, "//a[@href='/login']")
+sign_in_button.click()
